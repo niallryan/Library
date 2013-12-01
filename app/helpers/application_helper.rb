e01e1a3e672820e2da5e3e3a9951330a8002915c
Niall Ryan <ryann4@tcd.ie>
2013-12-01 19:26:06 +0000

    Updated Flash Notices, added helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  # http://teamtreehouse.com/library/build-a-simple-ruby-on-rails-application/designing-urls/refactoring-the-flash-code
+  def flash_class(type)
+    case type
+      when :alert
+        "alert-error"
+      when :notice
+        "alert-success"
+      else
+        ""
+    end
+  end
+
 end
