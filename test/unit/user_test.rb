f74a30adfcdc9d3a418228bc0c947d77ff80d40b
Niall Ryan <ryann4@tcd.ie>
2013-12-02 12:44:28 +0000

    Fixed test bug

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -20,12 +20,23 @@ class UserTest < ActiveSupport::TestCase
 
   # http://teamtreehouse.com/library/build-a-simple-ruby-on-rails-application/writing-tests/validating-format-2
   test "a user should have a profile name without spaces" do
-    user = User.new
+    user = User.new(email: "a_user@email.com")
+    user.password = user.password_confirmation = "password"
+
     user.profile_name = "A Name With Spaces"
 
     assert !user.save
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  # http://teamtreehouse.com/library/build-a-simple-ruby-on-rails-application/building-the-profile-page/testing-the-profile-name-2
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(email: "a_user@email.com")
+    user.password = user.password_confirmation = "password"
+
+    user.profile_name = "user_profile_name"
+    assert user.valid?
   end
 
 end
